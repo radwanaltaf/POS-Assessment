0c7ba991785cf7d0fae828cf7938be44894a111a
radwanaltaf <radwanaltaf2000@gmail.com>
2023-03-16 19:07:11 +0800

    Fetch data and display in table component

--- src/app/cities-table/cities-table.component.ts
+++ src/app/cities-table/cities-table.component.ts
@@ -9,19 +9,50 @@ import { CitiesService } from '../services/cities.service';
 })
 export class CitiesTableComponent implements OnInit {
   private citiesService: CitiesService;
-  showHideLoaderService: ShowHideLoaderService;
+  private showHideLoaderService: ShowHideLoaderService;
+  public citiesData: Array<Array<any>> = [];
+  public citiesDataRaw: Array<any> = [];
 
   constructor(private readonly injector: Injector) {
     this.citiesService = this.injector.get(CitiesService);
     this.showHideLoaderService = this.injector.get(ShowHideLoaderService);
   }
 
+  chunkArray(array: Array<Object>, chunkSize: number): Array<Array<Object>> {
+    if (!Array.isArray(array) || !Number.isInteger(chunkSize) || chunkSize <= 0) return [];
+    
+    const length = array.length;
+    const chunks = new Array(Math.ceil(length / chunkSize));
+  
+    for (let i = 0, j = 0; i < length; i += chunkSize, j++) {
+      chunks[j] = array.slice(i, i + chunkSize);
+    }
+  
+    // console.log(chunks);
+    return chunks;
+  }
+
+  showCityInfo(city: any): void {}
+
+  onCityClickHandler(event: any) {
+    console.log(event);
+  }
+
   ngOnInit(): void {
     this.showHideLoaderService.showLoaderAction();
-    this.citiesService.getCities().subscribe((data) => {
-      this.showHideLoaderService.hideLoaderAction();
-      console.log(this.showHideLoaderService.getLoaderStatus());
-      console.log('data', data);
-    }, (error) => { this.showHideLoaderService.hideLoaderAction(); });
+    this.citiesService.getCities().subscribe({
+      next: (data) => {
+        this.showHideLoaderService.hideLoaderAction();
+        const sortedData = data.sort((a: any, b: any) => a.name.localeCompare(b.name));
+        this.citiesDataRaw = sortedData;
+        this.citiesData = this.chunkArray(sortedData, 3);
+        console.log(this.citiesData);
+      },
+      error: (error) => { this.showHideLoaderService.hideLoaderAction() }
+    });
   }
+
+
+  
+
 }
